95177365e3f547db9743c43c5a4a84d7aad8a26b
fatih92 <f4t1h_1lg1@hotmail.com>
2018-11-03 11:06:22 +0300

    v2

--- hp_finder.py
+++ hp_finder.py
@@ -5,12 +5,15 @@ import os,sys,datetime
 #global_variables
 a = socket.gethostbyname_ex(socket.gethostname())[2]
 k = a[2]
+port_dict = {80:'http',21:'ftp',22:'ssh',23:'telnet',25:'smtp',443:'https'}
 def host_finder(ip,port):
     try:
         print("[*] Trying :" + str(port))
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((ip,port))
         print("[+] "+str(port)+":Open")
+        if port in port_dict:
+            print("Service : "+port_dict[port])
         s.close()
     except:
         print("[-] "+str(port)+": Closed")
@@ -20,18 +23,18 @@ if __name__ == '__main__':
     zaman = time.strftime("%H:%M:%S")
     print(zaman)
     if len(sys.argv) < 2:
-        options = input("Tarama Secenekleri \n[1]Local Ip Tara:{} \n[2]Remoto Ip Tara ".format(k))
+        options = input("Tarama Secenekleri \n[1]Local Ip Tara:{} \n[2]Remote Ip Tara \nOption:  ".format(k))
 
         if options == '1':
-            for i in range(80,81):
+            for i in port_dict.keys():
                 host_finder(k,i)
         elif options == '2':
             r = input("Remote Ip: ")
-            for i in range(79,81):
+            for i in port_dict.keys():
                 host_finder(r,i)
 
     else:
-        for i in range(79,81):
+        for i in port_dict.keys():
             host_finder(sys.argv[1],i)
 
     time2 = datetime.datetime.now()
